79809b739402e5af4731a4f40e7c90959757bfd3
yuvrajpawar09 <payuvraj62@gmail.com>
2026-09-18 17:01:27 +0530

    diagonal sum

---
+++ src/Arrays2D/diagonalsum.java
@@ -0,0 +1,22 @@
+package Arrays2D;
+
+public class diagonalsum {
+    public static int diagonaladd(int matrix[][]){
+        int sum = 0;
+        for (int i=0; i< matrix.length; i++){
+            sum+= matrix[i][i];
+
+            if(i != matrix.length-1-i){
+                sum+= matrix[i][matrix.length-1-i];
+            }
+        }
+        return sum;
+    }
+    public static void main(String[] args) {
+        int matrix[][] = {{1, 2, 3, 4,},
+                {5, 6, 7, 8},
+                {9, 10, 11, 12},
+                {13, 14, 15, 16}};
+        System.out.println(diagonaladd(matrix));
+    }
+}
